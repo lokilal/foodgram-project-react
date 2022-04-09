5891c8c88ab24349c037accfa9514b104597c669
Кирилл <k_kirill45@mail.ru>
2022-04-09 16:26:09 +0300

    Ссоздал миграции, удалил комменатарии

--- backend/foodgram_api/foodgram_api/settings.py
+++ backend/foodgram_api/foodgram_api/settings.py
@@ -3,23 +3,15 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '(i$$fr=lg=6z9weq#%tqzy0khyhxe%oi-ddi^t1!#!iz65^l1v'
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-
-# Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -67,9 +59,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'foodgram_api.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
     'default': {
