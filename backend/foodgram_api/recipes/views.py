2201a7b6e4351fbc36df4d95e99d0f40a33b1059
Кирилл <k_kirill45@mail.ru>
2022-04-21 11:42:35 +0300

    flake8

--- backend/foodgram_api/recipes/views.py
+++ backend/foodgram_api/recipes/views.py
@@ -3,11 +3,11 @@ from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import permissions, status, viewsets
 from rest_framework.decorators import action
-from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.response import Response
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
 from users.paginator import CustomPageNumberPaginator
+
 from .filters import IngredientsFilter, RecipeFilter
 from .models import (Favorite, Ingredient, Recipe, RecipeIngredient,
                      ShoppingList, Tag)
